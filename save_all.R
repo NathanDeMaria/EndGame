1734511d2d017338708a40708c5c9b32a7d2187e
NathanDeMaria <nathandmaria@gmail.com>
2018-03-14 00:42:28 -0500

    Save all the things

---
+++ save_all.R
@@ -0,0 +1,21 @@
+source('R/parse.R')
+source('R/progress.R')
+
+# First day of the 17-18 season
+season_tipoff <- as.Date('2017-11-10')
+
+this_season <- seq(season_tipoff, Sys.Date(), by = 1)
+data_dir <- 'data/'
+
+safely_save <- function(d) {
+  date_str <- as.character(d, '%Y%m%d')
+  cat('\n', date_str, '\n')
+  tryCatch({
+    events <- get_events(d, verbose = T)
+    saveRDS(events, sprintf('%s/%s.rds', data_dir, date_str))
+  }, error = function(e) {
+    cat(e, '\n')
+  })
+}
+
+this_season %>% map_with_progress(safely_save)
