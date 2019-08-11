4e91c8226a97676d7b18a0f4d03448e1bd064333
Nathan DeMaria <nathandmaria@gmail.com>
2019-08-11 15:13:47 -0500

    Add 247sports recuriting ratings

---
+++ 247_cfb.R
@@ -0,0 +1,47 @@
+library(tidyverse)
+library(EndGame)
+
+options(EndGame.cache_dir = './internet/')
+
+# Starts in 1999, but the numbers are garbage
+# Also, outside of top 50 didn't have ratings until 2002
+# and non-FBS didn't get them until 2012
+ratings <- map_df(seq(2000, 2019), get_247_fb_recuriting_ratings)
+
+# Team ratings by year
+# <50 2000-2001
+# All FBS(?) 2002-2011
+# Lots 2012+
+ratings %>% group_by(year) %>% summarise(rated_teams = sum(rating > 0)) %>%
+  ggplot() + geom_line(aes(x = year, y = rated_teams)) +
+  labs(x = 'Year', y = '# teams with non-zero ratings', title = 'Teams rated by year')
+
+# Total points by year is increasing, so I should probably normalize that...
+ratings %>% group_by(year) %>% summarise(total_points = sum(rating)) %>%
+  ggplot() + geom_line(aes(x = year, y = total_points))
+
+# Drop-off of ratings by ranking per year
+ratings %>% mutate(year = as.factor(year)) %>%
+  group_by(year) %>% mutate(rank = order(order(rating, decreasing = T))) %>%
+  ggplot() + geom_line((aes(x = rank, y = rating, col = year))) +
+  labs(x = 'Rank', y = 'Rating', title = 'Drop-off curves each year')
+
+# Drop-off of ratings by ranking per year
+ratings %>% group_by(year) %>% mutate(rank = order(order(rating, decreasing = T))) %>%
+  ggplot() + geom_line((aes(x = rank, y = rating, col = as.factor(year)))) +
+  labs(x = 'Rank', y = 'Rating', title = 'Drop-off curves each year')
+
+normalize <- function(x) {
+  (x - mean(x)) / sd(x)
+}
+
+# Same drop-off, but after normalizing
+ratings %>% mutate(year = as.factor(year)) %>%
+  group_by(year) %>% mutate(rating = normalize(rating)) %>%
+  mutate(rank = order(order(rating, decreasing = T))) %>%
+  ggplot() + geom_line((aes(x = rank, y = rating, col = year))) +
+  labs(x = 'Rank', y = 'Rating', title = 'Drop-off curves each year')
+
+# Yeah, I like that better. Gonna save normalized ratings
+ratings %>% group_by(year) %>% mutate(rating = normalize(rating)) %>%
+  write_csv('ncaaf_recruiting.csv')
--- R/get.R
+++ R/get.R
@@ -3,9 +3,9 @@ library(magrittr)
 library(stringr)
 
 
-get_cached <- function(url, query, cache_dir = getOption('EndGame.cache_dir', tempdir()), check_cache = T) {
-  url_string <- gsub('[:/\\.]', '', url)
-  param_string <- paste0(names(query), '=', query, collapse = '&')
+get_cached <- function(url, query, cache_dir = getOption('EndGame.cache_dir', tempdir()), check_cache = T, write_to_cache = T) {
+  url_string <- .clean_for_path(url)
+  param_string <- paste0(names(query), '=', query, collapse = '&') %>% .clean_for_path()
   saved_path <- file.path(cache_dir, paste0(url_string, param_string, '.rds'))
   if(file.exists(saved_path) & check_cache) {
     return(readRDS(saved_path))
@@ -17,6 +17,35 @@ get_cached <- function(url, query, cache_dir = getOption('EndGame.cache_dir', te
                            "Params: {param_string}"))
   }
   contents <-  response %>% content()
-  saveRDS(contents, saved_path)
+  if (write_to_cache) {
+    saveRDS(contents, saved_path)
+  }
   contents
+}
+
+
+get_cached_html <- function(url, query, cache_dir = getOption('EndGame.cache_dir', tempdir()), check_cache = T, write_to_cache = T) {
+  # Switching to saving as XML because of https://github.com/tidyverse/rvest/issues/181
+  # potentially only need this when you're loading/saving lists instead of full HTML pages
+  url_string <- .clean_for_path(url)
+  param_string <- paste0(names(query), '=', query, collapse = '&') %>% .clean_for_path()
+  saved_path <- file.path(cache_dir, paste0(url_string, param_string, '.html'))
+  if(file.exists(saved_path) & check_cache) {
+    return(read_html(saved_path))
+  }
+  response <- RETRY('GET', url, query = query, pause_base = 4, times = 10)
+  if(response$status_code != 200) {
+    stop(stringr::str_glue("Error: GET returned {response$status_code}. ",
+                           "URL: {url} ",
+                           "Params: {param_string}"))
+  }
+  contents <-  response %>% content()
+  if (write_to_cache) {
+    write_html(contents, saved_path)
+  }
+  contents
+}
+
+.clean_for_path <- function(s) {
+  gsub('[:/\\.]', '', s)
 }
