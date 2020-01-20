e66eb32e00cac0e225ef9ca7f921cd56edf584f6
Nathan DeMaria <nathandmaria@gmail.com>
2020-01-19 19:28:36 -0600

    Clickable warning url

--- py-endgame/endgame/web.py
+++ py-endgame/endgame/web.py
@@ -33,7 +33,7 @@ class CacheableContent:
 
 
 async def get(url: str, parameters: RequestParameters = None) -> CacheableContent:
-    param_string = '&'.join([f'{k}={v}' for k, v in parameters.items()]) if parameters else ''
+    param_string = _build_param_string(parameters)
     cache_path = re.sub('[:/\\.]', '', url + param_string)
     cache_file = Path(CONFIG.cache_dir, 'web', cache_path)
     if cache_file.is_file():
@@ -53,9 +53,11 @@ async def _get_with_retries(url: str, parameters: RequestParameters) -> bytes:
         except aiohttp.client_exceptions.ClientResponseError as e:
             if i + 1 == max_retries:
                 raise e
-            sleep_duration_s = (0.95  + 0.1 * random.random()) * ((i + 1) ** 2)
-            logger.warning(f"Struggling to get {url}. Status code {e.status}. Attempt number {i + 1}. Sleeping for {sleep_duration_s:.02f}")
             # Exponential backoff w/ +/- 10% jitter
+            sleep_duration_s = (0.95  + 0.1 * random.random()) * ((i + 1) ** 2)
+            param_string = _build_param_string(parameters)
+            full_url = f'{url}?{param_string}' if param_string else url
+            logger.warning(f"Struggling to get {full_url} Status code {e.status}. Attempt number {i + 1}. Sleeping for {sleep_duration_s:.02f}")
             await asyncio.sleep(sleep_duration_s)
 
 
@@ -63,3 +65,7 @@ async def _get_web(url: str, parameters: RequestParameters) -> bytes:
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params=parameters, raise_for_status=True) as response:
             return await response.read()
+
+
+def _build_param_string(parameters: RequestParameters) -> str:
+    return '&'.join([f'{k}={v}' for k, v in parameters.items()]) if parameters else ''
