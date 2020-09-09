58267347582ebad35898171220a85208a086cf10
Nathan DeMaria <nathandmaria@gmail.com>
2020-09-09 06:04:04 +0000

    Washington

--- py-endgame/endgame/nfl.py
+++ py-endgame/endgame/nfl.py
@@ -18,21 +18,47 @@ logger = getLogger(__name__)
 
 # Say each season ends on March 1st
 SEASON_END = (3, 1)
-BASE_URL = 'https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard'
-REAL_TEAMS = frozenset([
-    "Arizona Cardinals", "Atlanta Falcons", "Baltimore Ravens", "Buffalo Bills",
-    "Carolina Panthers", "Chicago Bears", "Cincinnati Bengals", "Cleveland Browns",
-    "Dallas Cowboys", "Denver Broncos", "Detroit Lions", "Green Bay Packers",
-    "Houston Texans", "Indianapolis Colts", "Jacksonville Jaguars", "Kansas City Chiefs",
-    "Los Angeles Chargers", "Los Angeles Rams", "Miami Dolphins", "Minnesota Vikings",
-    "New England Patriots", "New Orleans Saints", "New York Giants", "New York Jets",
-    "Oakland Raiders", "Philadelphia Eagles", "Pittsburgh Steelers", "San Francisco 49ers",
-    "Seattle Seahawks", "Tampa Bay Buccaneers", "Tennessee Titans", "Washington Redskins",
-])
+BASE_URL = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"
+REAL_TEAMS = frozenset(
+    [
+        "Arizona Cardinals",
+        "Atlanta Falcons",
+        "Baltimore Ravens",
+        "Buffalo Bills",
+        "Carolina Panthers",
+        "Chicago Bears",
+        "Cincinnati Bengals",
+        "Cleveland Browns",
+        "Dallas Cowboys",
+        "Denver Broncos",
+        "Detroit Lions",
+        "Green Bay Packers",
+        "Houston Texans",
+        "Indianapolis Colts",
+        "Jacksonville Jaguars",
+        "Kansas City Chiefs",
+        "Los Angeles Chargers",
+        "Los Angeles Rams",
+        "Miami Dolphins",
+        "Minnesota Vikings",
+        "New England Patriots",
+        "New Orleans Saints",
+        "New York Giants",
+        "New York Jets",
+        "Oakland Raiders",
+        "Philadelphia Eagles",
+        "Pittsburgh Steelers",
+        "San Francisco 49ers",
+        "Seattle Seahawks",
+        "Tampa Bay Buccaneers",
+        "Tennessee Titans",
+        "Washington",
+    ]
+)
 N_REGULAR_WEEKS = 17
 
 
-async def update(location: str = 'nfl.csv'):
+async def update(location: str = "nfl.csv"):
     end_year = get_end_year(SEASON_END)
     args = [[y] for y in range(1999, end_year + 1)]
     seasons = [s async for s in apply_in_parallel(get_season, args)]
@@ -41,7 +67,7 @@ async def update(location: str = 'nfl.csv'):
 
 async def get_season(year: int) -> Season:
     logger.info(f"Getting NFL season {year}")
-    cache = SeasonCache('nfl')
+    cache = SeasonCache("nfl")
     s = cache.check_cache(year)
     if s:
         return s
@@ -65,9 +91,9 @@ async def get_season(year: int) -> Season:
 async def get_week(season: int, week: int, season_type: SeasonType) -> Week:
     logger.info(f"Getting NFL {season} {season_type.name} week {week}")
     parameters: RequestParameters = dict(
-        lang='en',
-        region='us',
-        calendartype='blacklist',
+        lang="en",
+        region="us",
+        calendartype="blacklist",
         limit=32,
         seasontype=season_type.value,
         dates=season,
@@ -75,11 +101,7 @@ async def get_week(season: int, week: int, season_type: SeasonType) -> Week:
     )
 
     games = await get_games(BASE_URL, parameters)
-    games = [
-        _move_teams(g)
-        for g in games
-        if g.home in REAL_TEAMS
-    ]
+    games = [_move_teams(g) for g in games if g.home in REAL_TEAMS]
 
     if season_type == SeasonType.post:
         week += N_REGULAR_WEEKS
@@ -88,10 +110,14 @@ async def get_week(season: int, week: int, season_type: SeasonType) -> Week:
 
 def _move_teams(game: Game) -> Game:
     d = game.to_dict()
-    d['away'] = _move_team_name(d['away'])
-    d['home'] = _move_team_name(d['home'])
+    d["away"] = _move_team_name(d["away"])
+    d["home"] = _move_team_name(d["home"])
     return Game(**d)
 
 
 def _move_team_name(old_name: str) -> str:
-    return old_name.replace('San Diego', 'Los Angeles').replace('St. Louis', 'Los Angeles')
+    return (
+        old_name.replace("San Diego", "Los Angeles")
+        .replace("St. Louis", "Los Angeles")
+        .replace("Washington Redskins", "Washington")
+    )
