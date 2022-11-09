4489560c664a516ca8f14051cb988dbfe3153d4d
NathanDeMaria <nathandmaria@gmail.com>
2022-11-09 03:30:15 +0000

    Save possessions along with box scores

--- py-endgame-aws/endgame_aws/io.py
+++ py-endgame-aws/endgame_aws/io.py
@@ -1,4 +1,6 @@
 import pickle
+from csv import DictWriter
+from io import StringIO
 from aiobotocore.session import get_session
 from endgame.ncaabb.ncaabb import Season
 
@@ -11,3 +13,15 @@ async def save_to_s3(seasons: list[Season], bucket: str, key: str):
     session = get_session()
     async with session.create_client("s3") as client:
         await client.put_object(Bucket=bucket, Key=key, Body=dumped)
+
+
+async def save_csv_to_s3(data: list[dict], bucket: str, key: str):
+    with StringIO() as stream:
+        writer = DictWriter(stream, fieldnames=data[0].keys())
+        writer.writeheader()
+        writer.writerows(data)
+        body = stream.getvalue()
+
+    session = get_session()
+    async with session.create_client("s3") as client:
+        await client.put_object(Bucket=bucket, Key=key, Body=body)
--- py-endgame-aws/main.py
+++ py-endgame-aws/main.py
@@ -1,21 +1,29 @@
 from fire import Fire
 from endgame.ncaabb import NcaabbGender
 from endgame.ncaabb.ncaabb import _get_ncaabb_season
-from endgame_aws import save_to_s3, Config
+from endgame.ncaabb.matchup import logger, apply_in_parallel, get_possessions
+from endgame_aws import save_to_s3, save_csv_to_s3, Config
 
 
 _CONFIG = Config.init_from_file()
 
 
-async def main(gender_name: str):
+async def main(gender_name: str, year: int):
     gender = NcaabbGender[gender_name]
-    # save_box_scores(gender)
-    # save_possessions()
-    season = await _get_ncaabb_season(2021, gender)
+    season = await _get_ncaabb_season(year, gender)
 
-    # TODO: copy up the cache, too?
-    # Or will that get taken care of by EBS volume magic?
-    await save_to_s3([season], _CONFIG.bucket, f"seasons/{gender.name}.pkl")
+    await save_to_s3([season], _CONFIG.bucket, f"seasons/{year}/{gender.name}.pkl")
+
+    rows: list[dict] = []
+    for week in season.weeks:
+        logger.info("Getting matchups for %d %d", season.year, week.number)
+        args = [(gender, game.game_id) for game in week.games]
+        games = apply_in_parallel(get_possessions, args)
+        async for sides in games:
+            if sides is None:
+                continue
+            rows.extend(side.to_dict() for side in sides)
+    await save_csv_to_s3(rows, _CONFIG.bucket, f"seasons/{year}/{gender.name}.csv")
 
 
 if __name__ == "__main__":
